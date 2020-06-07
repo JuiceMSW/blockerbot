71f3d8a02950e411a1e18bb31cd332e38fe88aeb
supercarlos297 <footballshane@gmail.com>
2020-06-06 17:38:20 -0700

    redirects urls to our HTML

--- background.js
+++ background.js
@@ -1,5 +1,13 @@
 // Diplays the popup window on all websites when the extension is clicked
 
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+    return {
+        redirectUrl : chrome.extension.getURL("default.html")
+    };
+    }, {
+    urls : ["*://*.facebook.com/"]
+    }, ["blocking"]);
+
 chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
@@ -11,3 +19,6 @@ chrome.runtime.onInstalled.addListener(function() {
         }]);
     });
 });
+
+
+
